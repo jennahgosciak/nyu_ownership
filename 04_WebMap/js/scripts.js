a83e664b5c953c9695e7dcb87491d0006c6507b2
Jennah <jennahgosciak@gmail.com>
2021-12-03 00:56:00 -0500

    fix semicolon

--- 04_WebMap/js/scripts.js
+++ 04_WebMap/js/scripts.js
@@ -48,7 +48,7 @@ map.on('load',function(){
   // define a 'source' for your point dataset
   map.addSource('nyu_data',{
     'type':'geojson',
-    'data': "https://raw.githubusercontent.com/jennahgosciak/nyu_ownership/gh-pages/04_WebMap/data/nyu_test.geojson";
+    'data': "https://raw.githubusercontent.com/jennahgosciak/nyu_ownership/gh-pages/04_WebMap/data/nyu_test.geojson"
   });
   // add a new layer with your points
   map.addLayer({
